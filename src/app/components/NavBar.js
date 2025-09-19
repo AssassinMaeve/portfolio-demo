a3237fc5e923863f19b7a1136dd7c3f022a19eda
Maeve Fernandes <maeve.fernandes.7@gmail.com>
2025-09-19 15:12:21 +0530

    Nav bar created

---
+++ src/app/components/NavBar.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function NavBar(){
+    return(
+        <nav className="flex items-center justify-between px-20 py-4 bg-white text-black border-b border-gray-200">
+            <Link href='/' className="text-2xl font-bold justify-self-start ">My Portfolio</Link>
+
+            <div className="flex-grow flex justify-end gap-10 font-semibold">
+            <Link href='/' className="px-3 py-2 rounded-lg transition duration-300 hover:bg-black hover:text-white">Home</Link>
+            <Link href='/about' className="px-3 py-2 rounded-lg transition duration-300 hover:bg-black hover:text-white">About</Link>
+            <Link href='/projects' className="px-3 py-2 rounded-lg transition duration-300 hover:bg-black hover:text-white">Projects</Link>
+            <Link href='/contact' className="px-3 py-2 rounded-lg transition duration-300 hover:bg-black hover:text-white">Contact</Link>
+            </div>
+        </nav>
+    );
+}
