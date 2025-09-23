bc37238aff253fddf778b70867106c0195e75826
Maeve Fernandes <maeve.fernandes.7@gmail.com>
2025-09-23 10:30:43 +0530

    Optimised the cursor with invert color and fixed the color change background

--- src/app/components/CustomCursor.js
+++ src/app/components/CustomCursor.js
@@ -1,27 +1,41 @@
 "use client";
 import { useEffect, useState } from "react";
 
-export default function CustomCursor(){
-    const[position, setPosition] = useState({x:0, y:0});
+export default function CustomCursor() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [hovering, setHovering] = useState(false);
 
-    useEffect(()=>{
-        const mouseMove = (e) =>{
-            setPosition({x: e.clientX, y: e.clientY});
-        };
-        window.addEventListener("mousemove", mouseMove);
-        return () =>  window.removeEventListener("mousemove", updatePosition);
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener("mousemove", mouseMove);
 
-        
-    },[]);
+    const interactiveEls = document.querySelectorAll("a, button");
+    interactiveEls.forEach((el) => {
+      el.addEventListener("mouseenter", () => setHovering(true));
+      el.addEventListener("mouseleave", () => setHovering(false));
+    });
 
-    return(
-        <div 
-        className=" fixed top-0 left-0 w-8 h-8 bg-black rounded-full pointer-events-none mix-blend-difference transition-transform duration-150 ease-out"
-        style={{
-            transform: `translate3d(${position.x - 16}px, ${position.y - 16}px, 0)`,
-        }}
-        >
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+      interactiveEls.forEach((el) => {
+        el.removeEventListener("mouseenter", () => setHovering(true));
+        el.removeEventListener("mouseleave", () => setHovering(false));
+      });
+    };
+  }, []);
 
-        </div>
-    );
+  return (
+    <div
+      className={`fixed top-0 left-0 rounded-full pointer-events-none transition-all duration-200 ease-out z-[999999]`}
+      style={{
+        width: hovering ? "3rem" : "2rem",
+        height: hovering ? "3rem" : "2rem",
+        backgroundColor: "white",
+        mixBlendMode: "difference",
+        transform: `translate3d(${position.x - 24}px, ${position.y - 24}px, 0)`,
+      }}
+    />
+  );
 }
