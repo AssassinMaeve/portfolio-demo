165d91e4b12d0b774be126af3089e36bed9a8668
Maeve Fernandes <maeve.fernandes.7@gmail.com>
2025-09-23 15:46:55 +0530

    footer changes

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -29,25 +29,53 @@ export default function Footer() {
           : "opacity-0 translate-y-20"
       }`}
     >
-      <div className="max-w-6xl mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-4">Thanks for visiting ✨</h2>
-        <p className="text-lg text-gray-300">
-          &copy; {new Date().getFullYear()} My Portfolio. All rights reserved.
-        </p>
-      </div>
+      
 
       <div>
         {/* LEFT SIDE */}
-        <div className="max-w">
-          <p className="text-5xl pt-5 pl-20 font-semibold pb-25">
+        <div className="max-w pl-20">
+          <p className="text-5xl pt-5 font-semibold pb-5">
              Lorem ipsum dolor sit amet, consectetur Sed
             <br></br>Sed do eiusmod tempor incididunt minim<br></br>
           </p>
-           <button className="px-3 py-2   transition duration-300 bg-gray-100 text-1xl hover:bg-black hover:text-white">
+           
+        <div className="flex items-center gap-5">
+          <button className="px-6 py-2  text-black transition duration-300 bg-gray-100 text-1xl hover:bg-black hover:text-white">
             Get in Touch
         </button>
-        </div>
+        {/* Dot wrapper */}
+  <div className="relative w-3 h-3 ">
+    {/* Growing & fading halo */}
+    <span className="absolute inline-flex h-full w-full rounded-full bg-white opacity-100 animate-slow-ping"></span>
+    {/* Static dot */}
+    <span className="relative block w-3 h-3 bg-white rounded-full"></span>
+  </div>
+  <span className="text-white font-semibold ">Available Now</span>
       </div>
+        </div>
+
+
+      </div>
+      <div className="w-full mx-auto flex flex-col md:flex-row px-20 items-center justify-between text-l gap-4 md:gap-0 mt-15">
+  {/* Left: Contact Info */}
+  <div className="flex flex-col items-center md:items-start">
+    <span className="font-medium" href="tel:+918888888888">+91 88888 88888</span>
+    <span className="font-medium" href="mailto:example@example.com" >example@example.com</span>
+  </div>
+
+  {/* Center: Developer Credit */}
+  <div className="text-center font-medium">
+    Designed & Developed <br /> by Maeve Fernandes
+  </div>
+
+  {/* Right: Copyright */}
+  <div className="text-center md:text-right font-medium">
+    OPEN SOURCE, <br /> ASSASSINMAEVE ©2025
+  </div>
+</div>
+
+
+
 
      
 
